9dd124425ac02825801575aa44dfdfbfecc0bafe
Vinod <yadavv.2311@gmail.com>
2021-12-22 21:22:52 +0000

    implement blynk features

--- blynk.py
+++ blynk.py
@@ -3,9 +3,9 @@ import BlynkLib
 from sense_hat import SenseHat
 import time
 
-import blynk
+import blynk_config as conf
 
-BLYNK_AUTH = $BLYNK_AUTH_TOKEN
+BLYNK_AUTH = conf.BLYNK_AUTH_TOKEN
 
 # initialize Blynk
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
@@ -20,12 +20,15 @@ def v3_write_handler(value):
     buttonValue=value[0]
     print(f'Current button value: {buttonValue}')
     if buttonValue=="1":
-        sense.clear(255,255,255)
+        sense.show_message( "Stay", text_colour=[255,0,0])
     else:
         sense.clear()
+
+#def distance(value):
 
 #tmr_start_time = time.time()
 # infinite loop that waits for event
 while True:
+    exec(open("distance.py").read())
     blynk.run()
     time.sleep(1)
---
+++ distance.py
@@ -0,0 +1,23 @@
+#!/bin/sh
+from ble_scanner import ScanUtility
+import bluetooth._bluetooth as bluez
+
+#Set bluetooth device. Default 0.
+dev_id = 0
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print ("\n *** Looking for BLE Beacons ***\n")
+	print ("\n *** CTRL-C to Cancel ***\n")
+except:
+	print ("Error accessing bluetooth")
+
+ScanUtility.hci_enable_le_scan(sock)
+#Scans for iBeacons
+try:
+	while True:
+		returnedList = ScanUtility.parse_events(sock, 10)
+		for item in returnedList:
+			print(item)
+			
+except KeyboardInterrupt:
+    pass
